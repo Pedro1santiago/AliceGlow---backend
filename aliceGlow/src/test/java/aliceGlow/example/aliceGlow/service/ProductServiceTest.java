8a2a865aaf4938788f5af41be95e3d9cde9d88c1
Pedro1santiago <pedro.santigosiqueira@gmail.com>
2026-02-05 23:26:14 -0300

    add unit test for updating product successfully

--- aliceGlow/src/test/java/aliceGlow/example/aliceGlow/service/ProductServiceTest.java
+++ aliceGlow/src/test/java/aliceGlow/example/aliceGlow/service/ProductServiceTest.java
@@ -3,6 +3,8 @@ package aliceGlow.example.aliceGlow.service;
 import aliceGlow.example.aliceGlow.domain.Product;
 import aliceGlow.example.aliceGlow.dto.product.CreateProductDTO;
 import aliceGlow.example.aliceGlow.dto.product.ProductDTO;
+import aliceGlow.example.aliceGlow.dto.product.UpdateProductDTO;
+import aliceGlow.example.aliceGlow.dto.user.UserDTO;
 import aliceGlow.example.aliceGlow.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -53,6 +56,48 @@ class ProductServiceTest {
         verify(productRepository).save(any(Product.class));
 
     }
+
+    @Test
+    void shouldUpdateProductSuccessfully(){
+
+        Long productId = 1L;
+
+        UpdateProductDTO dto = new UpdateProductDTO(
+                "Mouse",
+                new BigDecimal("130.00"),
+                25
+        );
+
+        Product existingProduct = new Product();
+
+        existingProduct.setId(productId);
+        existingProduct.setName("Old Mouser Gamer");
+        existingProduct.setCostPrice(new BigDecimal("120.00"));
+        existingProduct.setStock(10);
+
+        Product updateProduct = new Product();
+
+        updateProduct.setId(productId);
+        updateProduct.setName("Mouse");
+        updateProduct.setCostPrice(new BigDecimal("130.00"));
+        updateProduct.setStock(20);
+
+        when(productRepository.findById(productId))
+                .thenReturn(Optional.of(existingProduct));
+        when(productRepository.save(any(Product.class)))
+                .thenReturn(updateProduct);
+
+        ProductDTO result = productService.updateProduct(productId, dto);
+
+        assertNotNull(result);
+        assertEquals("Mouse", result.name());
+        assertEquals(new BigDecimal("130.00"), result.costPrice());
+        assertEquals(20, result.stock());
+
+        verify(productRepository).findById(productId);
+        verify(productRepository).save(any(Product.class));
+    }
+
 
 
 
